ccacd45dc0df58837f3a9c59bd74209bb92085cd
Jainam Sheth <j4inam@gmail.com>
2025-03-23 20:28:15 -0700

    Bump header size

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,13 +13,17 @@ const loginSchema = z
   .required();
 
 const LoginPage = () => {
-  const { mutate: loginUser, isPending: isLoginPending, isSuccess: isLoginSuccess } = useLoginUser();
+  const {
+    mutate: loginUser,
+    isPending: isLoginPending,
+    isSuccess: isLoginSuccess
+  } = useLoginUser();
   const navigate = useNavigate();
 
   if (!isLoginPending && isLoginSuccess) {
     navigate({ to: '/search' });
   }
-  
+
   const loginForm = useForm({
     defaultValues: {
       name: '',
@@ -38,8 +42,8 @@ const LoginPage = () => {
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <PawPrint className="size-4" />
+            <div className="flex h-8 w-8 md:h-12 md:w-12 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <PawPrint className="size-6 md:size-8" />
             </div>
             Dog Fetch
           </a>
