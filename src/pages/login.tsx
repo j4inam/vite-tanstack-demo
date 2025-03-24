8fee60f64c43127ecf8dbefd5cb73551f497241e
Jainam Sheth <j4inam@gmail.com>
2025-03-23 20:30:19 -0700

    Bump brand name

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <a href="#" className="flex items-center gap-2 font-medium text-xl md:text-2xl lg:text-3xl">
             <div className="flex h-8 w-8 md:h-12 md:w-12 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <PawPrint className="size-6 md:size-8" />
             </div>
